5891fdaac8032218c8c8bf3fc45182411c5746bf
Sofyakarp1 <sofyakarp1@yandex.ru>
2021-02-15 01:44:21 +0300

    Первый коммит

---
+++ src/main/java/mirea/mosit/labs_mires/LabsMiresApplication.java
@@ -0,0 +1,13 @@
+package mirea.mosit.labs_mires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsMiresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabsMiresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mirea/mosit/labs_mires/LabsMiresApplicationTests.java
@@ -0,0 +1,13 @@
+package mirea.mosit.labs_mires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsMiresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
